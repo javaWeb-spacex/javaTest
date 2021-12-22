298a1a826fe6dcd28a03c4d87243bd66ea8ceaed
sbx <513274762@qq.com>
2021-12-22 23:55:40 +0800

    2021-12-21 IOC原理2

--- src/main/java/com/spacex/www/spring/ioc/introduction/Test.java
+++ src/main/java/com/spacex/www/spring/ioc/introduction/Test.java
@@ -2,6 +2,8 @@ package com.spacex.www.spring.ioc.introduction;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -12,14 +14,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Test {
     public static void main(String[] args) {
-        Logger logger= LoggerFactory.getLogger(Test.class);
+        Logger logger = LoggerFactory.getLogger(Test.class);
         //加载spring的配置文件
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring/introduction.xml");
-        User user =  context.getBean("User",User.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("/spring/introduction.xml");
+        User user = context.getBean("User", User.class);
         user.setName("sbx");
         user.setAge(25);
-       logger.info("Name:"+user.getName()+"Age:"+user.getAge());
-       user.print();
+        logger.info("Name:" + user.getName() + "Age:" + user.getAge());
+        user.print();
 
     }
 }
